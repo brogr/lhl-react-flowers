9b802ba7e4134b0c08274f6ccd3f52b0f5b04a5b
brogr <dev@brogramming.com>
2021-12-09 20:03:39 +0100

    day 1: create basic app with some content and styling

---
+++ src/Flower.js
@@ -0,0 +1,9 @@
+import flower from './bunchberry.jpeg';
+  
+const Flower = function () {
+  // const src = "bunchberry.jpeg";
+
+  return <img src={flower} alt="logo" className="logo" />;
+}
+
+export default Flower;
