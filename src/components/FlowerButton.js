c1c178cca358568c66c5c05629ce2a6d42a000b1
brogr <dev@brogramming.com>
2021-12-09 20:34:37 +0100

    added some comments

--- src/components/FlowerButton.js
+++ src/components/FlowerButton.js
@@ -1,5 +1,6 @@
 function FlowerButton(props) {
   return (
+    // onClick calls function in parent
     <button className="flowerButton" type="button" onClick={props.onClick}>
       {props.label}
     </button>
